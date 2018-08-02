55c879e1b6919795d2fa332b65af90f2dad196ae
Isaac Elliott <isaace71295@gmail.com>
2018-08-02 15:43:58 +1000

    Prepare for release

--- parsers-megaparsec.nix
+++ parsers-megaparsec.nix
@@ -1,8 +1,13 @@
-{ mkDerivation, base, megaparsec, mtl, parsers, stdenv, text }:
+{ mkDerivation, base, fail, megaparsec, mtl, parsers, semigroups
+, stdenv, text, transformers
+}:
 mkDerivation {
   pname = "parsers-megaparsec";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base megaparsec mtl parsers text ];
+  libraryHaskellDepends = [
+    base fail megaparsec mtl parsers semigroups text transformers
+  ];
+  description = "`parsers` instances for Megaparsec";
   license = stdenv.lib.licenses.bsd3;
 }
